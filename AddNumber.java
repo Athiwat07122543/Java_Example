6109c7f15d4373d3d45a4453eb9cbebf9a0a6ea6
Athiwat07122543 <Athiwat07122543@gmail.com>
2021-08-11 11:28:24 +0700

    Update Data 2

--- AddNumber.java
+++ AddNumber.java
@@ -1,7 +1,7 @@
 public class AddNumber {
-    public static voin main(Strig[] args){
+    public static void main(String[] args){
         int a = 3;
         int b = 5;
-        System.out,println("A + B = "+(a+b));
+        System.out.println("A + B = "+(a+b));
     }
 }
