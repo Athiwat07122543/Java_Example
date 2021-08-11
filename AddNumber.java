358838eb13945c66673ebf11b8cf573c247c6836
Athiwat07122543 <Athiwat07122543@gmail.com>
2021-08-11 11:23:44 +0700

    Update Data 2

---
+++ AddNumber.java
@@ -0,0 +1,7 @@
+public class AddNumber {
+    public static voin main(Strig[] args){
+        int a = 3;
+        int b = 5;
+        System.out,println("A + B = "+(a+b));
+    }
+}
--- MyFirstJava.java
+++ MyFirstJava.java
@@ -1,6 +1,7 @@
 public class MyFirstJava{
     public static void main(String[] args) {
-        System.out.println("Update data");
-        System.out.println("Git Command");
+        System.out.println("My name is Athiwat Dachmak");
+        System.out.println("ID: 624230047");
+        System.out.println("CLass: 62/38");
     }
 }
